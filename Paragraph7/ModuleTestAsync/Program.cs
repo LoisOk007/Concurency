7cf4b6a46347cdf4ab79cc30f583a80d4658772b
LoisOk007 <45103259+LoisOk007@users.noreply.github.com>
2024-04-02 11:09:01 +0300

    Параграф 7
    Добавил проект с реализацией примеров из 7 параграфа.

---
+++ Paragraph7/ModuleTestAsync/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModuleTestAsync;
+using Nito.AsyncEx;
+
+[TestMethod]
+void SmthAsync_False()
+{
+    AsyncContext.Run(async () =>
+    {
+        var value = new Sata(5);
+        bool results = await value.IsCountTwo();
+        Assert.IsFalse(results);
+    });
+}
---
+++ Paragraph7/ModuleTestAsync/Sata.cs
@@ -0,0 +1,17 @@
+﻿namespace ModuleTestAsync;
+
+public class Sata
+{
+    public int Value { get;}
+
+    public Sata(int value)
+    {
+        if (value != null)
+            Value = value;
+    }
+
+    public async IAsyncEnumerable<bool> IsCountTwo()
+    {
+        yield return Value % 2 == 0;
+    }
+}
